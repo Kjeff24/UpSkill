1c4674bb76fe43687a8cb24c824a1daa97358a44
Kjeff24 <jeffreyarthur123@gmail.com>
2023-11-07 03:21:11 -0800

    created user model

--- UpSkill/settings.py
+++ UpSkill/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'myapp',
 ]
 
+AUTH_USER_MODEL  = 'myapp.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
