daa9fe66d761d30dc5435d10d2f6c497f2f93d08
sweetboi <jamesnettey007@gmail.com>
2023-11-10 09:04:49 +0000

    ignore

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from .views import custom_404
+from employerAdmin.employer_admin import tutor_admin_site
+
+# NB exclude admin when hosting app
+
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("myapp.urls")),
+    path("", include("course.urls")),
+    path("", include("quiz.urls")),
+    path('tutor_admin/', tutor_admin_site.urls),
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
