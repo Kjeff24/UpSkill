f35e1ee14a260d96e72f605cef646387575bfaf1
sweetboi <jamesnettey007@gmail.com>
2023-11-10 07:42:16 +0000

    ok

--- employerAdmin/employer_admin.py
+++ employerAdmin/employer_admin.py
@@ -2,14 +2,14 @@ from django.contrib.admin import AdminSite
 
 class MyAdminSite(AdminSite):
     """
-    Custom admin site for SkillBuilder employers.
+    Custom admin site for U employers.
 
     :site_header: The header displayed on the admin site. :noindex:
     :site_title: The title displayed on the admin site. :noindex:
     :login_template: The template used for the admin login page. :noindex:
     """
     site_header = 'UpSkill Tutor Administration'
-    site_title = 'SkillBuilder Tutor Administration'
+    site_title = 'Upskill Tutor Administration'
     login_template = 'admin/login.html'
 
 tutor_admin_site = MyAdminSite(name='tutor_admin')
