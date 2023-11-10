88a1b87f8b882f8d966ed0888105ab044e6bceb7
sweetboi <jamesnettey007@gmail.com>
2023-11-10 09:04:29 +0000

    A

--- course/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .announcement_page import *
-from .course_page import *                              
-from .resource_page import *
-from .room_page import *
-from .chat_room import *
-from .quiz_page import *
-from .profile_page import *
